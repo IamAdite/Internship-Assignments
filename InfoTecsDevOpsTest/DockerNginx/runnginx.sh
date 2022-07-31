f942389f511e4711210fd27cf4b6ed055772016a
Pavel Ogoreltsev <paul.ogorelcev@gmail.com>
2022-07-31 21:00:13 +0500

    Update runnginx.sh

--- InfoTecsDevOpsTest/DockerNginx/runnginx.sh
+++ InfoTecsDevOpsTest/DockerNginx/runnginx.sh
@@ -6,55 +6,6 @@ docker build -t nginx-infotecs:1.0 .
 
 sleep 1
 
-echo "Creating nginx.conf"
-
-sleep 1
-
-cat <<EOT >> nginx.conf
-worker_processes  1;
-
-events {
-    worker_connections  1024;
-
-}
-
-http {
-
-    default_type  application/octet-stream;
-    
-    sendfile        on;
-    
-    keepalive_timeout  65;
-    
-    server {
-        root        /rootfs/var/www/html;
-    
-        listen       80;
-            
-        server_name  localhost;
-    
-        location / {
-            root   html;
-            index  index.html index.htm;
-        }
-
-        #error_page  404              /404.html;
-
-        error_page   500 502 503 504  /50x.html;
-        location = /50x.html {
-            root   html;
-        }
-
-    }
-}
-EOT
-
-sleep 1
-
-echo "nginx.conf created!"
-
-sleep 1
-
 echo "-------------------------------Run Container-------------------------------" 
 
 sleep 1
